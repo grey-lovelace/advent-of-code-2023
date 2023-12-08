c4900130dcbed6c8625eb3d58052a30953230710
Grey Lovelace <austin.grey.lovelace@gmail.com>
2023-12-08 13:08:25 -0600

    day08 refactor

--- src/days/day08/day08.ts
+++ src/days/day08/day08.ts
@@ -6,48 +6,48 @@ export default class Day08 extends Day {
   expectedPart1Results = () => [["sample.txt", 2], ["sample2.txt", 6]]
   expectedPart2Results = () => [["sample3.txt", 6]]
 
-  part1(input: string) {
-    const {directions, nodes} = parseDirsAndNodes(input)
-    let node = nodes["AAA"]
-    let i = 0
-    while (node.name !== "ZZZ") {
-      node = nodes[node[directions[i % directions.length]]]
-      i++
-    }
-    return i
-  }
+  part1 = (input: string) => findNumOfRuns(
+      input,
+      node => node.name === "AAA",
+      node => node.name === "ZZZ")
 
-  part2(input: string) {
-    const {directions, nodes} = parseDirsAndNodes(input)
-    return nodes
-      .values()
-      .filter(node => node.name.endsWith("A"))
-      .map(node => {
-        let i = 0
-        while (!node.name.endsWith("Z")) {
-          node = nodes[node[directions[i % directions.length]]]
-          i++
-        }
-        return i
-      })
-      .flatMap(primeFactors)
-      .unique()
-      .product()
-  }
+  part2 = (input: string) => findNumOfRuns(
+      input,
+      node => node.name.endsWith("A"),
+      node => node.name.endsWith("Z"))
 }
 
-const parseDirsAndNodes = (input: string) => input.lines()
-  .let(lines => ({
-    directions: lines[0].split("") as ("L" | "R")[],
-    nodes: lines.slice(2)
-      .map(l => l.matchAllAsList(/\w{3}/g)!
-        .let(matches => ({
-          name: matches[0][0],
-          L: matches[1][0],
-          R: matches[2][0],
-        })))
-      .associateBy(node => node.name)
-  }))
+class Node {constructor(public name: string, public L: string, public R: string){}}
+
+const findNumOfRuns = (
+  input: string,
+  isStartingNode: (node: Node) => boolean,
+  isEndingNode: (node: Node) => boolean
+) => input.lines()
+  .let(parseDirectionsAndNodes)
+  .let(({directions, nodeMap}) => nodeMap
+    .values()
+    .filter(isStartingNode)
+    .map(node => {
+      let i = 0
+      while (!isEndingNode(node)) {
+        node = nodeMap[node[directions[i % directions.length]]]
+        i++
+      }
+      return i
+    })
+    .flatMap(primeFactors)
+    .unique()
+    .product()
+  )
+
+const parseDirectionsAndNodes = (lines: string[]) => ({
+  directions: lines[0].split("") as ("L" | "R")[],
+  nodeMap: lines.slice(2)
+    .map(l => l.matchAllAsList(/\w{3}/g)!
+      .let(matches => new Node(matches[0][0], matches[1][0], matches[2][0])))
+    .associateBy(node => node.name)
+})
 
 if (import.meta.vitest) {
   tests(new Day08(), __dirname)
