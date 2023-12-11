a5dabfb0825dc704951599e00e6027a7c7376c8f
Grey Lovelace <austin.grey.lovelace@gmail.com>
2023-12-11 00:31:37 -0600

    day11

---
+++ src/days/day11/day11.ts
@@ -0,0 +1,44 @@
+import { range } from "d3"
+import Day from "../../utils/day"
+import { Grid, Point } from "../../utils/grid"
+import tests from "../../utils/testUtils"
+
+export default class Day11 extends Day {
+    expectedPart1Results = () => [["sample.txt", 374]]
+    expectedPart2Results = () => []
+
+    part1 = (input:string) => findDistances(input, 2)
+    part2 = (input:string) => findDistances(input, 1000000)
+}
+
+const findDistances = (input: string, expansionFactor: number) => {
+    const grid = input.lines()
+        .map(l => l.split(""))
+        .let(grid => new Grid(grid))
+
+    // Expand Space
+    const expansions = [
+        {max: grid.maxY, axis: "y"},
+        {max: grid.maxX, axis: "x"}
+    ] as const
+    expansions.forEach(({max, axis}) => range(0, max)
+        .filter(i => grid.points
+            .filter(point => point[axis] === i)
+            .every(point => point.val === "."))
+        .map((num, i) => num + i*(expansionFactor-1))
+        .forEach(rowNum => grid.points
+            .filter(point => point[axis] >= rowNum)
+            .forEach(point => point[axis] += expansionFactor - 1)))
+
+    // Find
+    const galaxies = grid.points.filter(point => point.val ==="#")
+    return galaxies.flatMap((g, i) => galaxies.slice(i+1).map(g2 => [g, g2]))
+        .map(pair =>
+            (Math.max(pair[0].x, pair[1].x)-Math.min(pair[0].x, pair[1].x))
+            + (Math.max(pair[0].y, pair[1].y)-Math.min(pair[0].y, pair[1].y)))
+        .sum()
+}
+
+if (import.meta.vitest) {
+    tests(new Day11(), __dirname)
+}
